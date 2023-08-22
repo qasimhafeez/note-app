f8061bf20530191616cd917675783eb370730064
qasimhafeez <qasimh556@gmail.com>
2023-08-22 20:44:02 +0400

    updated

--- routes/note.js
+++ routes/note.js
@@ -1,15 +1,19 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 const {
-	createNote,
-	getAllNotesForUser,
-	getNoteWithUser,
-} = require("../controller/note")
-const { verifyToken } = require("../middleware/auth")
+  createNote,
+  getAllNotesForUser,
+  getNoteWithUser,
+  deleteNote,
+  updateNote,
+} = require("../controller/note");
+const { verifyToken } = require("../middleware/auth");
 
-router.route("/").post(verifyToken, createNote)
-router.route("/:userId").get(verifyToken, getAllNotesForUser)
-router.route("/:noteId").get(verifyToken, getNoteWithUser)
+router.route("/").post(verifyToken, createNote);
+router.route("/:userId").get(verifyToken, getAllNotesForUser);
+router.route("/:noteId").get(verifyToken, getNoteWithUser);
+router.route("/:noteId").delete(verifyToken, deleteNote);
+router.route("/:noteId").put(verifyToken, updateNote);
 
-module.exports = router
+module.exports = router;
